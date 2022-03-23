8d576ee17f2507cd1172b9da793e0b5ce477556e
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2022-03-23 18:52:06 -0500

    added google fonts href link & src/pages src/components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import './App.css'
+import {Home} from './pages/index'
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <> 
+    <Home />
+    </>
   );
 }
 
