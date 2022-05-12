ee67477b024eb24bf54fedbf8ea2f36568c1c4d6
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2022-05-11 21:32:50 -0500

    slide arrow functions

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,37 +1,41 @@
-import { useState } from 'react'
-import { KeyboardArrowLeft, KeyboardArrowRight } from "@material-ui/icons";
+import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
+import { useState } from "react";
 import styled from "styled-components";
-import { sliderItems } from '../data';
+import { sliderItems } from "../data";
 
 const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
-  background-color: coral;
   position: relative;
+  overflow: hidden;
+ 
 `;
 
 const Arrow = styled.div`
-width: 50px;
-height: 50px;
-background-color: #fff7f7;
-border-radius: 50%;
-display: flex;
-align-items: center;
-justify-content: center;
-position: absolute;
-top: 0;
-bottom: 0;
-margin: auto;
-left: ${(props) => props.direction === "left" && "10px"};
-right ${(props) => props.direction === "right" && "10px"};
-cursor: pointer;
-opacity: 0.5;`;
+  width: 50px;
+  height: 50px;
+  background-color: #fff7f7;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+  margin: auto;
+  cursor: pointer;
+  opacity: 0.5;
+  z-index: 2;
+`;
 
 const Wrapper = styled.div`
   height: 100%;
+  display: flex;
   transition: all 1.5s ease;
-  transform: translateX(${(props) => props.slideIndex * -100}vw)
+  transform: translateX(${(props) => props.slideIndex * -100}vw);
 `;
 
 const Slide = styled.div`
@@ -39,14 +43,16 @@ const Slide = styled.div`
   height: 100vh;
   display: flex;
   align-items: center;
+  background-color: #${(props) => props.bg};
 `;
 
 const ImgContainer = styled.div`
   height: 100%;
+  width: 50%;
   flex: 1;
 `;
 
-const Image = styled.image`
+const Image = styled.img`
   height: 80%;
 `;
 
@@ -56,50 +62,54 @@ const InfoContainer = styled.div`
 `;
 
 const Title = styled.h1`
-font-size: 70px;`;
+  font-size: 70px;
+`;
 
 const Desc = styled.p`
-margin: 50px 0px;
-font-size: 20px;
-font-weight: 500;
-letter-spacing: 3px;`;
+  margin: 50px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
 
 const Button = styled.button`
-padding: 10px;
-font-size: 20px;
-background-color: transparent;
-cursor: pointer;`;
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
+`;
 
 const Slider = () => {
-  const [slideIndex, setSlideIndex] = useState(0)
+  const [slideIndex, setSlideIndex] = useState(0);
   const handleClick = (direction) => {
     if (direction === "left") {
-      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2)
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
     } else {
-      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0)
+      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
     }
-  }
+  };
+
   return (
     <Container>
       <Arrow direction="left" onClick={() => handleClick("left")}>
-        <KeyboardArrowLeft />
+        <ArrowLeftOutlined />
       </Arrow>
       <Wrapper slideIndex={slideIndex}>
         {sliderItems.map((item) => (
-                  <Slide bg={item.bg} key={item.id}>
-                  <ImgContainer>
-                    <Image src={item.img} />
-                  </ImgContainer>
-                  <InfoContainer>
-                    <Title>{item.title}</Title>
-                    <Desc>{item.desc}</Desc>
-                    <Button>Shop Now</Button>
-                  </InfoContainer>
-                </Slide>
+          <Slide bg={item.bg} key={item.id}>
+            <ImgContainer>
+              <Image src={item.img} />
+            </ImgContainer>
+            <InfoContainer>
+              <Title>{item.title}</Title>
+              <Desc>{item.desc}</Desc>
+              <Button>SHOW NOW</Button>
+            </InfoContainer>
+          </Slide>
         ))}
       </Wrapper>
       <Arrow direction="right" onClick={() => handleClick("right")}>
-        <KeyboardArrowRight />
+        <ArrowRightOutlined />
       </Arrow>
     </Container>
   );
