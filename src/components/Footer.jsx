b40b337faaba53a590d8fbdf669122d211f559b0
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2022-05-12 13:40:37 -0500

    prettier footer component pg

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import styled from "styled-components";
-import { Facebook, Instagram, Twitter, Phone, MailOutline } from "@material-ui/icons"
-
+import {
+  Facebook,
+  Instagram,
+  Twitter,
+  Pinterest,
+  Phone,
+  MailOutline,
+} from "@material-ui/icons";
 
 const Container = styled.div`
   display: flex;
@@ -17,27 +23,27 @@ const Left = styled.div`
 const Logo = styled.h1``;
 
 const Description = styled.p`
-    margin: 20px 0px;
+  margin: 20px 0px;
 `;
 
 const SocialContainer = styled.div`
-    display: flex;
+  display: flex;
 `;
 
 const SocialIcon = styled.div`
-    width: 40px;
-    height: 40px;
-    border-radius: 50%;
-    background-color: #${props => props.color};
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin-right: 20px;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
 `;
 
 const Center = styled.div`
   flex: 1;
-  padding: 20px;  
+  padding: 20px;
 `;
 const Title = styled.h3`
     margin-bottom: 30px;
@@ -46,16 +52,16 @@ const Title = styled.h3`
 `;
 
 const List = styled.ul`
-    margin: 0;
-    padding: 0;
-    list-style: none;
-    display: flex;
-    flex-wrap: wrap;
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
 `;
 
 const ListItem = styled.li`
-    width: 50%;
-    margin-bottom: 10px;
+  width: 50%;
+  margin-bottom: 10px;
 `;
 
 const Right = styled.div`
@@ -64,13 +70,13 @@ const Right = styled.div`
 `;
 
 const ContactItem = styled.div`
-    margin-bottom: 20px;
-    display: flex;
-    align-items: center;
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
 `;
 
 const Payment = styled.img`
-    width: 50%;
+  width: 50%;
 `;
 
 const Footer = () => {
@@ -86,30 +92,39 @@ const Footer = () => {
           <SocialIcon color="e4405f">
             <Instagram />
           </SocialIcon>
-          <SocialIcon color="e60023">
+          <SocialIcon color="55ACEE">
             <Twitter />
+          </SocialIcon>
+          <SocialIcon color="E60023">
+            <Pinterest />
           </SocialIcon>
         </SocialContainer>
       </Left>
       <Center>
-          <Title>Useful Links</Title>
-          <List>
-            <ListItem>Home</ListItem>
-            <ListItem>Cart</ListItem>
-            <ListItem>Mens Fashion</ListItem>
-            <ListItem>Womens Fashion</ListItem>
-            <ListItem>Accessories</ListItem>
-            <ListItem>My Account</ListItem>
-            <ListItem>Order Tracking</ListItem>
-            <ListItem>Wishlist</ListItem>
-            <ListItem>Terms</ListItem>
-          </List>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Mens Fashion</ListItem>
+          <ListItem>Womens Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
       </Center>
       <Right>
-          <Title>Contact</Title>
-          <ContactItem><MailOutline style={{marginRight:"10px"}}/>nickpbab@gmail.com</ContactItem>
-          <ContactItem><Phone style={{marginRight:"10px"}}/>337 257-9202</ContactItem>
-          <Payment src="https://i.ibb.co/Qfvn4z6/payment.png"/>
+        <Title>Contact</Title>
+        <ContactItem>
+          <MailOutline style={{ marginRight: "10px" }} />
+          nickpbab@gmail.com
+        </ContactItem>
+        <ContactItem>
+          <Phone style={{ marginRight: "10px" }} />
+          337 257-9202
+        </ContactItem>
+        <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
       </Right>
     </Container>
   );
